72fe900db71c5d89ce5588f8311d35fca6f6afb7
Moh. Syaifudin <71805620+syaiifudin@users.noreply.github.com>
2021-07-02 09:39:34 +0700

    up pagination

--- cobaiklanku/resources/views/frontend/layouts/listloker.blade.php
+++ cobaiklanku/resources/views/frontend/layouts/listloker.blade.php
@@ -132,6 +132,23 @@ https://templatemo.com/tm-537-art-factory
                     </div>
                 </div>
             </div>
+            <!-- ***** Pagination Start ***** -->
+            <nav aria-label="...">
+                <ul class="pagination justify-content-center">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Prev</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">2</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                  </li>
+                </ul>
+              </nav>
+            <!-- ***** Pagination End ***** -->
         </div>
     </section>
     <!-- ***** Features Small End ***** -->
